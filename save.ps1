3a7f93d9acfca49665d3a09a57d33a20ef9ae324
Aaron Huinink <a_huinink@hotmail.ca>
2024-02-28 14:57:00 -0800

    made save script

--- save.ps1
+++ save.ps1
@@ -1,8 +1,9 @@
 git add .
-$msg = Read-Host "Enter commit msg:"
-if ($msg == "")
+$msg = Read-Host "Enter commit msg"
+if ($msg -eq "")
 {
 	Write-Host "commit msg null - aborting..."
+	Exit
 }
 git commit -m $msg
 git push
